0804430db9690ff5cc11c36ffceb024cb9474edd
MR-HU <270296401@qq.com>
2013-11-17 16:19:16 +0800

    add WelcomeActivity

---
+++ src/com/zjut/express/activity/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.zjut.express.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
