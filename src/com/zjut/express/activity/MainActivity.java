2330a8ff8c764f25a45b84d785d5541631869d70
MR-HU <270296401@qq.com>
2013-11-17 16:14:11 +0800

    modify MainActivity

--- src/com/zjut/express/activity/MainActivity.java
+++ src/com/zjut/express/activity/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println();
+		System.out.println("Hello World");
 	}
 
 	@Override
